dcf40248ab0a462c64f7be18800e52ba5bef9346
SanjeevThalod <thalodsanjeev@gmail.com>
2023-08-20 20:32:53 +0530

    u

--- src/Components/SingleChat.js
+++ src/Components/SingleChat.js
@@ -26,8 +26,8 @@ export default function SingleChat({ fetchAgain, setFetchAgain }) {
 
   useEffect(() => {
     socket = io(ENDPOINT);
-    socket.emit('setup', user);
     socket.on('connected', () => {
+      socket.emit('setup', user);
       setSocketConnected(true);
     });
     socket.on('typing', () => setIsTyping(true));
